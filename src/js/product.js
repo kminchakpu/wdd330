6958c3b9725499da02a54db8f0adf9f029cfe010
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-06-29 13:20:41 +0100

    Update product.js
    Found and fix the error that override products in the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,20 @@
-import { setLocalStorage } from "./utils.mjs";
+// Import getLocalStorage from utils.mjs
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the current cart array from localStorage, 
+  const currentCart = getLocalStorage("so-cart") || [];
+
+  // Push the new product object into the array
+  currentCart.push(product);
+
+  // Save the updated array back to localStorage
+  setLocalStorage("so-cart", currentCart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
