ade1bc95a7fdc1eff9a23aad2db180dea0568a76
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-16 10:35:37 +0100

    Fix the title of the product listing page
    This is to make the category of the selected product appear on the title.

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -6,9 +6,21 @@ import updateCartCount from "./cartCount.mjs";
 loadHeaderFooter();
 
 const category = getParam("category") || "tents";
+// Update page heading
+const title = document.getElementById("categoryTitle");
+const formattedCategory = category
+  .replace("-", " ")
+  .replace(/\b\w/g, (letter) => letter.toUpperCase());
+
+title.textContent = `Top Products: ${formattedCategory}`;
+
 const dataSource = new ProductData();
 const listElement = document.querySelector(".product-list");
-const productList = new ProductList(category, dataSource, listElement);
+const productList = new ProductList(
+  category,
+  dataSource,
+  listElement
+);
 
 productList.init();
 updateCartCount();
