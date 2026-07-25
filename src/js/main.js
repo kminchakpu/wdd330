044554dabacbc912eb3382ddd1cedc65fcfcb397
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-25 15:58:54 +0100

    Add newsletter signup feature

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ import initRegisterModal from "./registerModal.js";
 import initSearch from "./search.js";
 import updateCartCount from "./cartCount.mjs";
 import Alert from "./Alert.js";
+import initNewsletter from "./newsletter.js";
 
 async function init() {
   await loadHeaderFooter();
   await initRegisterModal();
   await initSearch();
+  await initNewsletter();
   await updateCartCount();
 
   const alert = new Alert();
